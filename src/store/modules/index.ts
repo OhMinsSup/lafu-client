f354844152f1661dd95f2ade798f557158aa725d
오민섭 <you@example.com>
2019-11-26 21:34:13 +0900

    redux error,user store 생성

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import user from './user';
+import error from './error';
 
 const rootReducer = combineReducers({
-
+    user,
+    error
 })
 
 export type StortState = ReturnType<typeof rootReducer>
